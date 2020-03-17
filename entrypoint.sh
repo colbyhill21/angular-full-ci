1faa4d42462d3151d6ddeaa5e43f33e6817eba1b
Colby Hill <32140554+colbyhill21@users.noreply.github.com>
2020-03-17 08:31:16 -0400

    Added printed stages for each phase

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+
+echo "\n**Installing Dependencies**\n"
 npm install
-npm install -g @angular/cli --latest
+
+echo "\n**Auditing Packages**\n"
 npm audit
+
+echo "\n**Installing Angular CLI**\n"
+npm install -g @angular/cli --latest
+ng config -g cli.warnings.versionMismatch false
+
+echo "\n**Linting Code**\n"
 ng lint
+
+echo "\n**Building Project**\n"
 ng build --prod
+
+echo "\n**Running Unit Tests**\n"
 ng test --watch=false --browsers=ChromeHeadlessCustom
